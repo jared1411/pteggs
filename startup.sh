bf8db179097cb554792397e69406aaefe42191ea
jared1411 <55896591+jared1411@users.noreply.github.com>
2024-08-25 00:21:58 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,7 +3,6 @@ echo "Path Of Titans Egg by Marcel Baumgartner"
 echo "(c) Copyright 2022 endelon-hosting.de"
 echo "Checking for updates"
 ./AlderonGamesCmd --game path-of-titans --server true --beta-branch $BETA_BRANCH --install-dir .
-echo "Starting server"
 UE_TRUE_SCRIPT_NAME=$(echo \"$0\" | xargs readlink -f)
 UE_PROJECT_ROOT=$(dirname "$UE_TRUE_SCRIPT_NAME")
 chmod +x "$UE_PROJECT_ROOT/PathOfTitans/Binaries/Linux/PathOfTitansServer-Linux-Shipping"
